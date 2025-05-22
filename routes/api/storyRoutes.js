64a70ac44b0daf893e0dea0d242a6f61f818a03e
gaurav chopra <chopragc03@gmail.com>
2025-05-22 11:05:57 +0530

    change name of storyLikes

--- routes/api/storyRoutes.js
+++ routes/api/storyRoutes.js
@@ -2,14 +2,14 @@ const addStory = require("../../controllers/auth/story/addStory");
 const getStories = require("../../controllers/auth/story/getStories");
 const getDetailStories = require("../../controllers/auth/story/getDetailStories")
 const checkAuth = require("../../middleware/checkAuth");
-const storyLikes = require("../../controllers/auth/story/StoryLikes");
+const storyLikes = require("../../controllers/auth/story/storyLikes")
 
 const route = require("express").Router();
 
 route.post("/add",checkAuth,addStory);
 route.get("/getstories",getStories);
 route.get("/:id",getDetailStories);
-route.put("/like/:id",checkAuth,storyLikes);
+route.put("/like/:id",checkAuth, storyLikes);
 
 module.exports=route;
 
